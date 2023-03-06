3354d3bebccefa5a678f96324338dc20b5d63206
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-03-06 21:09:08 +0100

    update project

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+
+}
+
+const AboutPage = ({}) => {
+  return (
+    <div>Cześć Marcinku!</div>
+  )
+}
+
+export default AboutPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Marcin Marcin &nbsp;
+            Marcin Marcin yyy &nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
