ca7985a66fcfe23b36582ecee89cb94174eb93ed
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-03-06 22:40:17 +0100

    adding / at the end to the URL path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: function () {
+    return {
+      '/about': { page: '/about' },
+    }
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
+// module.exports = {
+//   reactStrictMode: true,
+//   exportPathMap: function () {
+//     return {
+//       '/about': { page: '/about' },
+//     }
+//   },
+//   trailingSlash: true, // <-- dodaj trailingSlash
+// }
