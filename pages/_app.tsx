8f61bb43dd423041294b9466f563ecc39621fd8f
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-03-26 20:07:30 +0200

    update project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <p className="text-5xl text-red-500">Hej kurła</p>
+    <Component {...pageProps} />
+  </div>
 }
