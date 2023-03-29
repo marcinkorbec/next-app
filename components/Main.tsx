d63aa28306cb9232ae2f800ca076a26ce671a0d9
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-03-30 00:04:05 +0200

    update project

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,7 +1,19 @@
 export const Main = () => {
   return (
-    <main className="flex-grow py-1 px-2">
-      Właściwa zawartość.
+    <main className="flex-grow bg-amber-50 grid sm:grid-cols-2">
+      <img src="https://www.jednoslad.pl/wp-content/uploads/2021/02/Triumph-Speed-Twin.jpeg" alt="triumph"/>
+      <p className="p-10">Współczesny, bajerancki i lekki – oto kilka głównych powodów, dla których Speed Twin 900 jest
+        najpopularniejszym klasycznym customowym motocyklem marki Triumph.
+        <br/>
+        <br/>
+        Napędzany najnowszej generacji silnikiem Bonneville o wysokim momencie obrotowym i poj. 900 cm³, zaktualizowanym
+        na rok 2021, z jego unikatowym charakterem, wyróżniającym się brzmieniem, czystą customową stylistyką i
+        łatwością prowadzenia sprawia, że jazda nim jest po prostu wciągająca. Speed Twin 900 ma genialny styl i jest
+        wygodny, dzięki czemu jest idealnym Bonneville'em dla współczesnego motocyklisty. Model ten jest najbardziej
+        popularnym klasycznym customowym motocyklem marki Triumph, a wraz z premierą udoskonalonego Speed Twina 900
+        sprawdzamy, dlaczego ten kultowy, nowoczesny motocykl jest tak lubiany oraz jakie funkcje na 2021 rok sprawiają,
+        że będzie on najlepszą generacją w historii.
+        Quasi, sequi ut.</p>
     </main>
   )
 }
