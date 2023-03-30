837d6eb473b9161996b644616771fabf47615fc8
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-03-30 15:32:54 +0200

    update project

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,42 @@
 import Link from "next/link";
+import {useEffect, useState} from "react";
 
+const links = [
+  { href: '/', label: 'Strona główna' },
+  { href: '/about/', label: 'O nas' },
+  { href: '/blog/', label: 'Blog' }
+];
 export const Header = () => {
+  const [selected, setSelected] = useState("");
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    setSelected(path);
+  }, [selected])
+
+  const mappedLinks = () => {
+    return links.map(({ href, label }) => (
+      <>
+        <Link key={href} href={href} passHref>
+            <span className={`mr-3.5 ml-3.5 ${selected === href ? "active" : ""}`} onClick={() => setSelected(href)}>
+              {label}
+            </span>
+        </Link>
+        <p className="inline">|</p>
+      </>
+    ))
+  }
+
   return (
     <header>
       <nav className="bg-amber-600 py-1 px-2 text-white">
-        <Link className="py-4" href="/">Strona główna</Link>
-        <Link href="/about"> O nas</Link>
+        {mappedLinks()}
       </nav>
+      <style jsx>{`
+        .active {
+          color: red;
+        }
+      `}</style>
     </header>
-  )
-}
+  );
+};
---
+++ pages/blog.tsx
@@ -0,0 +1,15 @@
+import {Header} from "@/components/Header";
+import {Footer} from "@/components/Footer";
+import React from "react";
+
+const Blog = () => {
+  return (
+    <>
+      <Header/>
+      <div>Users 2</div>
+      <Footer/>
+    </>
+
+  )
+}
+export default Blog;
