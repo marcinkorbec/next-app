e92cc63427a4f8aa159ab637dc406ef575039103
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-05-07 23:43:07 +0200

    Add tailiwind elements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css';
+import "tw-elements/dist/css/tw-elements.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
