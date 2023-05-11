791e9f5bbe57dabf71f178e69b54468e659fcd87
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-05-11 23:56:10 +0200

    4.2: SSG in practice

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,4 +1,19 @@
+export interface ProductApiResponse {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
 const ProductPage = () => {
   return <div>Siemka</div>;
 }
-export default ProductPage
+export default ProductPage;
+
+export const getStaticProps = async () => {};
