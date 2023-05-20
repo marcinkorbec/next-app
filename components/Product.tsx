655eadab5165228c8199ca0c5207104790414744
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-05-20 23:02:58 +0200

    4.8

--- components/Product.tsx
+++ components/Product.tsx
@@ -13,7 +13,7 @@ interface Props {
 export const Product = ({data}: Props) => {
   return (
     <>
-      <Image className='p-4' src={data.thumbnailUrl} alt={data.thumbnailAlt}/>
+      <img className='p-4' src={data.thumbnailUrl} alt={data.thumbnailAlt}/>
       <h2 className='p-4 text-3xl font-bold'>{data.title}</h2>
       <p className="p-4">{data.description}</p>
       <p className='p-4 text-sky-500	font-bold'>{data.rating}</p>
