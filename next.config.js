1d20675e2a6f72109b4241c2129979793038cc93
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-05-20 23:02:41 +0200

    4.8

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
     }
   },
   trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
