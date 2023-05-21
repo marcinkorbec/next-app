8a95a6f36a118fa654a0a73f444f7daeeddb2191
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-05-21 23:52:18 +0200

    4.9: Pobieranie treści po stronie klienta (CSR) i React Query

--- pages/product-csr.tsx
+++ pages/product-csr.tsx
@@ -34,23 +34,23 @@ const ProductCSRPage = () => {
     return <div>Coś poszło nie tak...</div>
   }
   return (
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {
-          data.map((product: ProductApiResponse) => {
-            return (
-              <li key={product.id} className='shadow-xl border-2'>
-                <Product data={{
-                  title: product.title,
-                  description: product.description,
-                  thumbnailUrl: product.image,
-                  thumbnailAlt: product.title,
-                  rating: product.rating.rate,
-                }}/>
-              </li>
-            );
-          })
-        }
-      </ul>
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {
+        data.map((product: ProductApiResponse) => {
+          return (
+            <li key={product.id} className='shadow-xl border-2'>
+              <Product data={{
+                title: product.title,
+                description: product.description,
+                thumbnailUrl: product.image,
+                thumbnailAlt: product.title,
+                rating: product.rating.rate,
+              }}/>
+            </li>
+          );
+        })
+      }
+    </ul>
   )
 }
 export default ProductCSRPage;
